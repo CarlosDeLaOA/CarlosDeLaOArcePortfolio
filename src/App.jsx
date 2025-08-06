7c714b15347548366dcf1d1a3312e7e8ed188326
CarlosDeLaOA <cdelaoa@ucenfotec.ac.cr>
2025-08-06 17:47:59 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,8 +66,9 @@ function App() {
   <div className="card cv-buttons">
     <p>Puedes descargar mi currículum en tu idioma preferido:</p>
     <div className="cv-button-group">
-      <a href="https://carlosdelaoa.github.io/CarlosDeLaOArcePortfolio/cv-esp.pdf" download className="cv-btn">📄 CV Español</a>
-<a href="https://carlosdelaoa.github.io/CarlosDeLaOArcePortfolio/cv-eng.pdf" download className="cv-btn">📄 CV Inglés</a>
+      <a href="https://carlosdelaoa.github.io/CarlosDeLaOArcePortfolio/cv-esp.pdf" target="_blank" rel="noopener noreferrer" className="cv-btn">📄 CV Español</a>
+      <a href="https://carlosdelaoa.github.io/CarlosDeLaOArcePortfolio/cv-eng.pdf" target="_blank" rel="noopener noreferrer" className="cv-btn">📄 CV Ingles</a>
+
 
 
     </div>
